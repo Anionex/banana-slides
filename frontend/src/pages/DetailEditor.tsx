682f5f7e75087c5dd23f9bd61dbd72d215e5c795
Anion <1005128408@qq.com>
2026-02-06 13:03:33 +0800

    feat: support pasting images in description editor
    Add clipboard image paste handler to the description card edit modal.
    Pasted images are uploaded as materials and inserted as markdown image
    links at the cursor position.

--- frontend/src/pages/DetailEditor.tsx
+++ frontend/src/pages/DetailEditor.tsx
@@ -326,6 +326,7 @@ export const DetailEditor: React.FC = () => {
                     key={pageId}
                     page={page}
                     index={index}
+                    projectId={currentProject.id}
                     onUpdate={(data) => updatePageLocal(pageId, data)}
                     onRegenerate={() => handleRegeneratePage(pageId)}
                     isGenerating={pageId ? !!pageDescriptionGeneratingTasks[pageId] : false}
