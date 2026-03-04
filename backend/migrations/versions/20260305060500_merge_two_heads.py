2a02f9f1de2a8a8e0c9ae18d808aff9d9e6a211c
Anion <1005128408@qq.com>
2026-03-05 05:58:57 +0800

    fix(migrations): merge two heads (9ad736fec43d, a1cb9099a491)

---
+++ backend/migrations/versions/20260305060500_merge_two_heads.py
@@ -0,0 +1,24 @@
+"""merge two heads
+
+Revision ID: e0f1a2b3c4d5
+Revises: 9ad736fec43d, a1cb9099a491
+Create Date: 2026-03-05 06:05:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e0f1a2b3c4d5'
+down_revision = ('9ad736fec43d', 'a1cb9099a491')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
