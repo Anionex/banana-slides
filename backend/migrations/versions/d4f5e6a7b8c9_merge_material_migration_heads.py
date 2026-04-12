106696583c55c9b64524a1463fdd7766bbb4e391
Hinton Ting <HintonTing@gmail.com>
2026-04-12 16:21:07 +0800

    fix(migrations): merge deploy and material heads

---
+++ backend/migrations/versions/d4f5e6a7b8c9_merge_material_migration_heads.py
@@ -0,0 +1,21 @@
+"""merge material migration heads
+
+Revision ID: d4f5e6a7b8c9
+Revises: c1d2e3f4a5b6, c153f8c4e111
+Create Date: 2026-04-12 16:20:00.000000
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = 'd4f5e6a7b8c9'
+down_revision = ('c1d2e3f4a5b6', 'c153f8c4e111')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
