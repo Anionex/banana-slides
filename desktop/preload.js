e3015d08916c003978d5a6d09a482c17ee30a0e8
Anion <1005128408@qq.com>
2026-04-26 23:03:58 +0800

    feat(desktop): add preload script with IPC bridge

---
+++ desktop/preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+  getPlatform: () => process.platform,
+  getBackendPort: () => ipcRenderer.invoke('get-backend-port'),
+  minimizeWindow: () => ipcRenderer.send('window-minimize'),
+  maximizeWindow: () => ipcRenderer.send('window-maximize'),
+  closeWindow: () => ipcRenderer.send('window-close'),
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+});
